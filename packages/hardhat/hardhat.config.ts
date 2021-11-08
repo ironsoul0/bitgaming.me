c3d262975ea3717395c16efbd7073715b814294a
Temirzhan Yussupov <timka2609@gmail.com>
2021-11-08 19:38:41 +0600

    feat: add correct configs

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,16 +1,20 @@
 import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import "dotenv/config";
 
 import { HardhatUserConfig } from "hardhat/types";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "localhost",
   solidity: "0.8.3",
-  paths: {
-    artifacts: "../frontend/artifacts",
-  },
+  // networks: {
+  //   ropsten: {
+  //     url: process.env.ROPSTEN_URL,
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  // },
   typechain: {
-    outDir: "../frontend/types/typechain",
+    outDir: "./types/typechain",
     target: "ethers-v5",
   },
 };
