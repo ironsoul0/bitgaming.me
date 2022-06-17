5bea111b31a6a28a04ddff5a7c8501c1df96ca8c
Temirzhan Yussupov <timka2609@gmail.com>
2022-06-18 00:04:39 +0800

    feat: add getUsers() for leaderboard functionality

--- packages/hardhat/contracts/BITToken.sol
+++ packages/hardhat/contracts/BITToken.sol
@@ -3,9 +3,18 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+library Leaderboard {
+  struct User {
+    address addr;
+    uint256 balance;
+  }
+}
+
 contract BITToken is ERC20 {
   address public owner;
   address public burner;
+  mapping(address => bool) existingUsers;
+  address[] public holders;
 
   constructor(uint256 initialSupply) ERC20("Brain Index Token", "BIT") {
     owner = msg.sender;
@@ -22,8 +31,23 @@ contract BITToken is ERC20 {
     _;
   }
 
+  function getUsers() external view returns (Leaderboard.User[] memory) {
+    Leaderboard.User[] memory users = new Leaderboard.User[](holders.length);
+
+    for (uint256 i = 0; i < holders.length; i++) {
+      users[i].addr = holders[i];
+      users[i].balance = balanceOf(holders[i]);
+    }
+
+    return users;
+  }
+
   function claimTokens(uint256 tokens) external {
     _mint(msg.sender, tokens);
+    if (!existingUsers[msg.sender]) {
+      holders.push(msg.sender);
+    }
+    existingUsers[msg.sender] = true;
   }
 
   function burnTokens(address target, uint256 tokens) external onlyBurner {
