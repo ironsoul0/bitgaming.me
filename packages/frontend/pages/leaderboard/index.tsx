f9b0f16f96883591425dfca6d8788fc9ef7758bb
AkezhanR <rakishev-as@ext.sber-zvuk.com>
2022-06-18 20:44:52 +0600

    feat: mock user avatars

--- packages/frontend/pages/leaderboard/index.tsx
+++ packages/frontend/pages/leaderboard/index.tsx
@@ -3,58 +3,6 @@ import React from "react";
 
 import { VerticalNavigationTemplate } from "../../components";
 
-export const UserRow: React.FC<any> = ({
-  name,
-  address,
-  score,
-  avatar,
-  className,
-  index,
-}: any) => {
-  const default_avatar =
-    "https://pwco.com.sg/wp-content/uploads/2020/05/Generic-Profile-Placeholder-v3.png";
-
-  return (
-    <div
-      className={clsx([
-        "flex justify-between h-10 mx-2 justify-between",
-        className,
-      ])}
-    >
-      <div className="flex flex-col items-start justify-center">
-        <div className="flex items-center grid grid-cols-12 gap-2">
-          <div className="overflow-hidden col-span-1">
-            <p className="flex flex-col text-white truncate align-middle text-md font-regular">
-              {index}
-            </p>
-          </div>
-          <div className="flex flex-col items-center justify-center ml-1 col-span-2">
-            <div
-              className="bg-gray-300 rounded-full h-9 w-9"
-              style={{
-                backgroundImage:
-                  "url(" + (avatar ? avatar : default_avatar) + ")",
-                backgroundSize: "cover",
-              }}
-            />
-          </div>
-          <div className="overflow-hidden col-span-9">
-            <p className="flex flex-col text-white truncate align-middle text-md font-regular">
-              {name}
-            </p>
-            <p className="flex flex-col text-sm text-gray-400 truncate align-middle font-regular">
-              {address.substr(0, 10).concat("...")}
-            </p>
-          </div>
-        </div>
-      </div>
-      <p className="flex items-center justify-end w-32 py-1 pr-4 text-xs font-bold text-white border-2 border-gray-400 rounded-md">
-        {score?.toFixed(1)} BIT
-      </p>
-    </div>
-  );
-};
-
 const leaderboard = [
   {
     id: 1,
@@ -88,6 +36,91 @@ const leaderboard = [
   },
 ];
 
+const users = [
+  {
+    name: "Alibek.eth",
+    avatar: "/av1.png",
+  },
+  {
+    name: "Rauan.eth",
+    avatar: "/av2.png",
+  },
+  {
+    name: "Ulan.eth",
+    avatar: "/av3.png",
+  },
+  {
+    name: "Sanzhar.eth",
+    avatar: "/av4.png",
+  },
+  {
+    name: "Temirzhan.eth",
+    avatar: "/av5.png",
+  },
+  {
+    name: "Akezhan.eth",
+    avatar: "/av6.png",
+  },
+  {
+    name: "Khafiz.eth",
+    avatar: "/av7.png",
+  },
+  {
+    name: "Daulet.eth",
+    avatar: "/av8.png",
+  },
+];
+
+const UserRow: React.FC<any> = ({
+  name,
+  address,
+  score,
+  avatar,
+  className,
+  index,
+}: any) => {
+  const default_avatar =
+    "https://pwco.com.sg/wp-content/uploads/2020/05/Generic-Profile-Placeholder-v3.png";
+
+  return (
+    <div className={clsx(["flex h-10 mx-2 justify-between", className])}>
+      <div className="flex flex-col items-start justify-center">
+        <div className="flex items-center grid grid-cols-12 gap-2">
+          <div className="overflow-hidden col-span-1">
+            <p className="flex flex-col text-white truncate align-middle text-md font-regular">
+              {index}
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center ml-1 col-span-2">
+            <div
+              className="w-10 h-10 bg-gray-300 rounded-full"
+              style={{
+                backgroundImage:
+                  "url(" + (avatar ? avatar : default_avatar) + ")",
+                backgroundSize: "cover",
+              }}
+            />
+          </div>
+          <div className="ml-2 overflow-hidden col-span-9">
+            <p className="flex flex-col text-white truncate align-middle text-md font-regular">
+              {name}
+            </p>
+            <p className="flex flex-col text-sm text-gray-400 truncate align-middle font-regular">
+              {address.substr(0, 10).concat("...")}
+            </p>
+          </div>
+        </div>
+      </div>
+      <p
+        style={{ borderColor: "#784FFE", color: "#E7DFFF" }}
+        className="flex items-center justify-center w-32 py-1 py-4 text-xs font-bold border-2 rounded-md"
+      >
+        {score?.toFixed(1)} BIT
+      </p>
+    </div>
+  );
+};
+
 const LeaderboardPage = () => {
   return (
     <>
@@ -111,7 +144,10 @@ const LeaderboardPage = () => {
                   leaderboard.map((user, index) => (
                     <UserRow
                       key={user.id}
-                      {...user}
+                      name={users[index].name}
+                      avatar={users[index].avatar}
+                      score={user.score}
+                      address={user.address}
                       index={index + 1}
                       className="mb-4"
                     />
